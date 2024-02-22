ea908cc2f8180ddf481848b947f5d1884dc58ed4
Maudit00 <davide9311@gmail.com>
2024-02-22 18:18:23 +0100

    fixing

---
+++ src/test/java/it/epicode/gestione_eventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
